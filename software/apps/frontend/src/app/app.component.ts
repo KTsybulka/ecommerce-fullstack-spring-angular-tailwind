89955c14d2fd7e3f3ac2663361ec87cb637da5bd
KTsybulka <ktsybulka11@gmail.com>
2025-01-02 13:37:08 -0400

    Refine navbar styles and layout

--- software/apps/frontend/src/app/app.component.ts
+++ software/apps/frontend/src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FaConfig, FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { fontAwsomeIcons } from './shared/font-awesome-icons';
+import { NavbarComponentComponent } from "./layout/navbar/navbar.component";
+import { FooterComponentComponent } from "./layout/footer/footer.component";
 
 @Component({
-  imports: [ RouterModule],
-  selector: 'app-root',
+  imports: [RouterModule, FontAwesomeModule, NavbarComponentComponent, FooterComponentComponent],
+  selector: 'ecom-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit{
+  private faIconLibrary = inject(FaIconLibrary); // Inject FontAwesome library
+  private faConfig = inject(FaConfig); // Inject FontAwesome config
+
+  ngOnInit(): void {
+    this.initFontAwesome(); // Initialize FontAwesome
+  }
+
+  private initFontAwesome(): void {
+    this.faConfig.defaultPrefix = 'far'; // Set the default prefix for icons
+    this.faIconLibrary.addIcons(...fontAwsomeIcons); // Add your icons
+  }
 }
---
+++ software/apps/frontend/src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,9 @@
+// imports specific items from Font Awesome icon libraries and creates a 
+// list of icons for use in an application
+
+import { IconDefinition } from "@fortawesome/angular-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+
+export const fontAwsomeIcons: IconDefinition[] = [
+    faUser,
+];
